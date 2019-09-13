ca250ca12649f8b23aa4bb2edf329c40df3535b9
joe <joe@ubuntu-base.cs.dixie.edu>
2019-09-13 09:00:18 -0600

    updated application.java to load on tomcat run

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
